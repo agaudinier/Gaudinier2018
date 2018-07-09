208d08cee5f0c7c81fff9f343bbc1947926a5f02
agaudinier <agaudinier@ucdavis.edu>
2018-07-09 16:27:32 -0700

    Update ElementalAnalysis.R

--- ElementalAnalysis.R
+++ ElementalAnalysis.R
@@ -1,16 +1,21 @@
+##Elemental Analysis
+
+##read in data
 Set1 = read_csv("~/Desktop/WholePlantPhenotyping/ElementalAnalysis/ElementalAnalysis.csv")
 
+#check dataset
 head(Set1)
 
 library(tidyverse)
-          
+
+#look at boxplots of all data
 boxplot(C_per_ug ~ Genotype, data = Set1)
 
 boxplot(N_per_ug ~ Genotype, data = Set1)
 
 boxplot(C_to_N ~ Genotype, data = Set1)
 
-nac032 <- filter(Set1, Genotype = Col-0, nac032-1))
+##filter data for Col-0 vs T-DNA ANOVAs
 
 #### nac032-1
 nac032 <- Set1 %>% 
@@ -156,67 +161,5 @@ CN_lm <- stats::lm(C_to_N ~ Genotype , data = nlp7)
 summary(aov(CN_lm))
 
 
-chart <- filter(Set1 , !(Genotype == "chl1-5"))
 
-geno <- c("nlp7-1", "arf18-2", "nac032-1", "Col-0",  "arid5-1", "arf18-3", "myb29-1", "rav2-1", "lbd4-1", 
-          "erf107-1", "hmgb15-1",  "bbx16-1")
-          
-chart$Genotype <- factor(chart$Genotype, levels = geno)
-
-library(ggplot2)
-
-ggplot (data = chart, mapping = aes( x = Genotype, y = C_per_ug, fill = Genotype)) +
-  geom_boxplot(position = position_dodge(width=0.9),outlier.shape=NA) +
-  geom_point(position=position_jitterdodge(dodge.width=.99), size = .75, alpha =.7) +
-  scale_fill_manual(values=c( "gray", "white", "white", "white", "white", 
-                              "white", "white", "white", "white", "white", "white", "white" )) +
-  labs(y = "% 13C per ug") +
-  theme_light() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) +
-  theme(legend.position="none") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  
-  
-  "gray", "white", "white", "#309acc", "#309acc","#309acc",
-  "#309acc", "#309acc", "#bee8ff")) + 
-  
-  
-  geno <- c("rav2-1", "lbd4-1", "arf18-2", "arid5-1","myb29-1", "arf18-3", "nac032-1", "erf107-1",       
-              "bbx16-1","hmgb15-1","Col-0",   "nlp7-1")
-  
-  chart$Genotype <- factor(chart$Genotype, levels = geno)
-  
-  ggplot (data = chart, mapping = aes( x = Genotype, y = N_per_ug, fill = Genotype)) +
-  geom_boxplot(position = position_dodge(width=0.9),outlier.shape=NA) +
-  geom_point(position=position_jitterdodge(dodge.width=.99), size = .75, alpha =.7) +
-  scale_fill_manual(values=c( "gray", "white", "#9ACC30", "white", "white", 
-                              "white", "white", "white", "white", "white", "white", "white" )) +
-  labs(y = "% 15N per ug") +
-  theme_light() +
-  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) +
-  theme(legend.position="none") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  
-  
-  ##c/n ratio
-  
-  library(ggplot2)
-  
-  
-  
-geno <- c("Col-0", "nlp7-1", "rav2-1", "arf18-2","nac032-1", "arf18-3", "erf107-1", "arid5-1", "hmgb15-1", 
-          "bbx16-1","lbd4-1",  "myb29-1")
-  
-  chart$Genotype <- factor(chart$Genotype, levels = geno)
-  
-  ggplot (data = chart, mapping = aes( x = Genotype, y = C_to_N, fill = Genotype)) +
-    geom_boxplot(position = position_dodge(width=0.9),outlier.shape=NA) +
-    geom_point(position=position_jitterdodge(dodge.width=.99), size = .75, alpha =.7) +
-    scale_fill_manual(values=c( "gray", "gold", "white", "white", "white", 
-                                "white", "white", "white", "white", "white", "white", "white" )) +
-    labs(y = "13C/15N ratio") +
-    theme_light() +
-    theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank()) +
-    theme(legend.position="none") +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
   
